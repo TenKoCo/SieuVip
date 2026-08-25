20b1ebfdedea364cedaee7bda8ff6313067e8eb2
Sieu <51594915+TenKoCo@users.noreply.github.com>
2026-08-25 19:58:11 +0700

    Create sieuvipsu.sh

---
+++ sieuvipsu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg install python-pip && pkg install python tsu libexpat openssl -y && pip install requests Flask colorama aiohttp psutil crypto pycryptodome loguru prettytable 
+curl -Ls "https://raw.githubusercontent.com/TenKoCo/SieuVip/refs/heads/main/sieuvip.py" -o /sdcard/Download/sieuvip.py
